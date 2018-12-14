0218c31a2e2c4b5bdaefb9c85e11738dcf82bea0
Ojo Tokunbo <ojo_tokunbo2002@yahoo.com>
2018-12-14 10:54:55 +0100

    testing

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,7 +15,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
-        'name' => 'test test',
+        'name' => 'test tests',
         'email' => 'test@test.com',
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
